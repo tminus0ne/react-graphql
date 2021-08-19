a1c308324dc5a6e428292fb32d933297557a8dce
Greg-One <sgmdoc@yandex.ru>
2021-08-19 20:41:07 +0500

    add Movie schema

--- server/app.js
+++ server/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 
+const schema = require('./schema/schema');
+
 const PORT = process.env.PORT || 5005;
 
 const app = express();
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  }),
+);
 
 app.listen(PORT, (err) => {
   err

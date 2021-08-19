1236469cd96dc6ba7baa1439e651b566a6ea889b
Greg-One <sgmdoc@yandex.ru>
2021-08-19 21:53:56 +0500

    add second data array & arrays interaction

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,15 +1,23 @@
 const {
   GraphQLObjectType,
   GraphQLString,
+  GraphQLInt,
   GraphQLSchema,
   GraphQLID,
 } = require('graphql');
 
 const movies = [
-  { id: 1, name: 'Pulp Fiction', genre: 'Crime' },
-  { id: 2, name: '1984', genre: 'Sci-fi' },
-  { id: 3, name: 'V for vendetta', genre: 'Sci-fi' },
-  { id: 4, name: 'Snatch', genre: 'Crime-comedy' },
+  { id: 1, name: 'Pulp Fiction', genre: 'Crime', directorId: 1 },
+  { id: 2, name: '1984', genre: 'Sci-fi', directorId: 2 },
+  { id: 3, name: 'V for vendetta', genre: 'Sci-fi', directorId: 3 },
+  { id: 4, name: 'Snatch', genre: 'Crime-comedy', directorId: 4 },
+];
+
+const directors = [
+  { id: 1, name: 'Tarantino', age: 55 },
+  { id: 2, name: 'Radford', age: 72 },
+  { id: 3, name: 'McTeigue', age: 51 },
+  { id: 4, name: 'Ritchie', age: 50 },
 ];
 
 const MovieType = new GraphQLObjectType({
@@ -18,6 +26,21 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find((director) => director.id == parent.id);
+      },
+    },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -29,6 +52,14 @@ const Query = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return movies.find((movie) => movie.id == args.id);
+      },
+    },
+
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return directors.find((director) => director.id == args.id);
       },
     },
   },
